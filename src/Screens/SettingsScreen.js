8bebb03b46c41decf80ed2dfd0ab66fb9992c6bb
CataTorres22 <catalina.torresxi@gmail.com>
2025-04-12 08:18:43 -0500

    SettingsScreen.js

--- src/Screens/SettingsScreen.js
+++ src/Screens/SettingsScreen.js
@@ -6,3 +6,16 @@ import {updateEmail,updatePassword, updateProfile} from 'firebase/auth'
 import {showMessage} from 'react-native-flash-message'
 import {useNavigation} from '@react-navigation/native'
 import * ImagePicker from 'expo-image-picker'
+
+const   CLOUDINARY_URL= 'https://api-cloudinary.com/v1_1/dvt9eesc4/image/upload'
+const UPLOAD_PRESET= 'IMAGE_CITAPRO'
+
+const SettingsScreen =() =>{
+  const navigation = useNavigation();
+  const {user, setUser}=useAuth();
+  const [imageUri. setImageUri]= useState(null);
+  const defaultImage= require
+  conts[isModalVisible, setModalVisible]= useState(false)
+  const[modalTitle,setModalTitle]= useState('')
+  const [FieldValue,setFieldValue]= useState('')
+}
